0f2b52861e91bf54bd7bc4f5e66490057123fdbd
aylok1n <nikolya.blog@gmail.com>
2022-04-02 15:39:41 +0400

    Create point on map

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <AppBar sx={{ alignItems: "center", position: 'static', marginBottom: 2 }}>
-        <Toolbar sx={{ width: '100%', maxWidth: 800 }} >
+        <Toolbar sx={{ width: '100%', maxWidth: 1000 }} >
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Детали заказа
           </Typography>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,23 @@
 import { useRef, useEffect, useState } from 'react';
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import '../index.css'
+import axios from 'axios';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXlsb2sxbiIsImEiOiJjbDE0ZmRramswbWFrM2JtdGYwbnU2d2d3In0.QRmUHL8-H_ExM2o2p2AEBw'
+
 
 export default function Map() {
     const [map, setMap] = useState<mapboxgl.Map>();
     const mapNode = useRef<HTMLDivElement>(null);
     const [center, setCenter] = useState([37.617633, 55.755820])
+
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken
+    })
+    let userMarker: mapboxgl.Marker | null = null
 
     //init map
     useEffect(() => {
@@ -20,33 +29,52 @@ export default function Map() {
             setCenter([longitude, latitude])
         })
 
-        const mapboxMap = new mapboxgl.Map({
+        setMap(new mapboxgl.Map({
             container: node,
             style: "mapbox://styles/aylok1n/cl14g5yku002r14rkia9s9csi",
             accessToken: 'pk.eyJ1IjoiYXlsb2sxbiIsImEiOiJjbDE0ZmRramswbWFrM2JtdGYwbnU2d2d3In0.QRmUHL8-H_ExM2o2p2AEBw',
-            center: [37.617633, 55.755820],
-            zoom: 9,
-
-        });
-        setMap(mapboxMap);
-
-        mapboxMap.on('click', onClickHandler)
+            zoom: 13,
+        }));
 
         return () => {
-            mapboxMap.remove();
+            map?.remove();
         };
     }, []);
 
+    // set map center 
     useEffect(() => {
-        map?.setCenter({ lng: center[0], lat: center[1] })
-    }, [center])
+        if (map) {
+            map.setCenter({ lng: center[0], lat: center[1] })
+            map.on('click', onClickHandler)
+        }
+    }, [map, center])
 
-
+    const createMarker = (lngLat: mapboxgl.LngLat) => {
+        if (map) {
+            if (userMarker) {
+                userMarker.remove()
+            }
+            userMarker = new mapboxgl.Marker({ color: 'yellow' })
+            userMarker.setLngLat(lngLat).addTo(map)
+        }
+    }
 
     const onClickHandler = (event: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
+        // create user marker
+        const { lng, lat } = event.lngLat
 
-        if (map) new mapboxgl.Marker({ color: 'FFFF00' }).setLngLat(event.lngLat).addTo(map)
+        createMarker(event.lngLat)
 
+        axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${mapboxgl.accessToken}`)
+            .then(res => {
+                console.log(res.data)
+                if (res.status === 200) {
+                    const place = res.data.features[0]
+                    if (place) {
+                        createMarker({ lng: place.center[0], lat: place.center[1] } as mapboxgl.LngLat)
+                    }
+                }
+            })
     }
 
 
